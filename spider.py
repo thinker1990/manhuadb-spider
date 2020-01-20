95cd98c752f015a4655f5cd95cc1278b66eb99f9
Michael Liu <thinker1990@users.noreply.github.com>
2020-01-20 11:18:32 +0800

    Refactoring

--- spider.py
+++ spider.py
@@ -1,6 +1,7 @@
 from config import *
 from checkpoint import *
 from utilities import httpclient, directory, cli
+from typing import Iterable
 
 
 def search(key_word):
@@ -18,11 +19,12 @@ def crawl(comic, start_page):
     directory.create_and_cd(comic)
     volumes = _get_volumes(start_page)
 
-    @cli.progressbar
-    def process_each(items):
-        for volume, link in items:
+    @cli.progressbar(total=len(volumes), desc='Total Progress')
+    def crawl_all_volumes():
+        for volume, link in volumes:
             yield crawl_volume(volume, link)
-    process_each(volumes)
+
+    crawl_all_volumes()
 
 
 def crawl_volume(volume, link):
@@ -32,11 +34,12 @@ def crawl_volume(volume, link):
     directory.create(volume)
     pages = _get_pages(link)
 
-    @cli.progressbar
-    def process_each(items):
-        for page in items:
+    @cli.progressbar(total=len(pages), desc=volume)
+    def crawl_all_pages():
+        for page in pages:
             yield crawl_page(volume, link, page)
-    process_each(pages)
+
+    crawl_all_pages()
 
 
 @checkpoint_when_abort
@@ -50,10 +53,11 @@ def crawl_page(volume, link, page):
 
 
 @resume_volume
-def _get_volumes(link):
+def _get_volumes(link) -> Iterable:
     return query_volumes(link)
 
 
 @resume_page
-def _get_pages(link):
-    return page_count(link)
+def _get_pages(link) -> Iterable:
+    total_page = page_count(link)
+    return range(1, total_page+1)
